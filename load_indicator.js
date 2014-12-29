b1e8e0787a2d67d449087c5c5e16f56319b732eb
JoelHearne <joel.hearne@gmail.com>
2014-12-29 13:23:54 -0600

    Added timeout handler
    Added timeout handler in effort to prevent the "neverending loading"
    icon.

--- load_indicator.js
+++ load_indicator.js
@@ -1,14 +1,21 @@
 define([
 	'dojo/_base/declare',
+	'dojo/topic',
 	'dijit/_WidgetBase',
 	'dojo/dom',
+	'dojo/dom-attr',
 	"dojo/dom-construct",
 	"dojo/_base/window",
 	'dojo/on',
 	'dojo/_base/lang',
 	'dojox/lang/functional',
+    'dojo/_base/array',
+    "dojo/query",
+    "dojo/ready",
 	'esri/geometry/Extent',
-], function ( declare, _WidgetBase,dom,domConstruct, win,  on, lang,array,Color, functional, Extent) {
+	"dojo/domReady!"
+], function ( declare,topic, _WidgetBase,dom,domAttr,domConstruct, win,  on, lang,array,Color, functional,
+  array,query,ready,Extent) {
 		return declare([_WidgetBase], {
         postCreate: function () {
             this.inherited(arguments);
@@ -18,11 +25,25 @@ define([
             dojo.place(img_html,"waitdiv");
 
 			this.map.on('update-start', function (layer) {
+				//console.log("load_indicator update-start");
 				dojo.byId("waitdiv").style.visibility="visible";
+				setTimeout( function(){   dojo.byId("waitdiv").style.visibility="hidden"; } , 7000);
 			});
 			this.map.on('update-end', function (layer) {
-				dojo.byId("waitdiv").style.visibility="hidden";
+				//console.log("update-end");
+ 				dojo.byId("waitdiv").style.visibility="hidden";
 			});
+
+
+
+/*
+			topic.subscribe('layerControl/setVisibleLayers', function (r) {
+				console.log('-----layerControl/setVisibleLayers------'); //layer id
+				console.log(r.id); //layer id
+				console.log(r.visibleLayers); //array of set visible layer ids
+			});
+*/
+
         } ,
         startup: function () {
 			 this.inherited(arguments);
@@ -34,5 +55,6 @@ define([
              this.map.infoWindow.show(mapPoint);
              */
         }
+
    });  // end return
 });  // end declare function
